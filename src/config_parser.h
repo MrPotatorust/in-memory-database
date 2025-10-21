53cd518eeb281e1c3ccb987af0f1de6bb85f3524
Martin <bumko96@gmail.com>
2025-10-21 08:06:00 +0200

    added config_parser enum and union

--- src/config_parser.h
+++ src/config_parser.h
@@ -1,20 +1,30 @@
 #include <stdbool.h>
 
-#ifndef CONFIG_PARSER 
+#ifndef CONFIG_PARSER
 #define CONFIG_PARSER
-union configValue {
-    int number;
+union configValue
+{
+    int integer;
     bool boolean;
     char string[30];
 } configValue;
+
+enum valueType
+{
+    integer,
+    boolean,
+    string
+};
 
 typedef struct configItem
 {
     char key[30];
 
+    enum valueType valueType;
+
+    union configValue value;
+
 } configItem;
-
-
 
 int parse();
 
